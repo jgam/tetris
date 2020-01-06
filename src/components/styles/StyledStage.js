e1b5cca4399ce5e8b1d1e6ae2ef3eafc5b6d45cb
jgam <jgam@alumni.nd.edu>
2020-01-06 22:25:17 +0900

    styling

---
+++ src/components/styles/StyledStage.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledStage = styled.div`
+  display: grid;
+  grid-template-rows: repeat(
+    ${props => props.height},
+    calc(25vw / ${props => props.width})
+  );
+  grid-template-columns: repeat(${props => props.width}, 1fg);
+`;
