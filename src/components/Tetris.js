11d6678c33530fb6a917a397ef7a2aa2e144f971
jgam <jgam@alumni.nd.edu>
2020-01-07 21:57:09 +0900

    basic game done

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import { createStage, checkCollision } from '../gameHelpers';//why we need createStage? due to rendering?
+import { createStage, checkCollision } from '../gameHelpers'; //why we need createStage? due to rendering?
 
 // styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 //custom hooks
-import {usePlayer} from '../hooks/usePlayer';
-import {useStage} from '../hooks/useStage';
+import { useInterval } from '../hooks/useInterval';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
 
 // Components
 import Stage from './Stage';
@@ -15,83 +16,103 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 const Tetris = () => {
-    const [dropTime, setDropTime] = useState(null);//dropTime is null
-    const [gameOver, setGameOver] = useState(false);//gameOver is false
+  const [dropTime, setDropTime] = useState(null); //dropTime is null
+  const [gameOver, setGameOver] = useState(false); //gameOver is false
 
-    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();//player's position with tetromino
-    const [stage, setStage] = useStage(player, resetPlayer);//stage created?
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer(); //player's position with tetromino
+  const [stage, setStage] = useStage(player, resetPlayer); //stage created?
 
-    console.log('re-render');
-    //used hooks with bunch of move functions. Need to elaborate on these
-    const movePlayer = dir => {
-        if(!checkCollision(player, stage, {x:dir, y:0})){
-          updatePlayerPos({x:dir, y:0});
-        }
+  console.log('re-render');
+  //used hooks with bunch of move functions. Need to elaborate on these
+  const movePlayer = dir => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
     }
+  };
 
-    const startGame = () => {
-        //reset everything
-        setStage(createStage());
-        resetPlayer();
-        setGameOver(false);
-    }
+  const startGame = () => {
+    //reset everything
+    setStage(createStage());
+    setDropTime(1000);
+    resetPlayer();
+    setGameOver(false);
+  };
 
-    const drop = () => {
-      if(!checkCollision(player, stage, {x:0, y:1})){
-        updatePlayerPos({x:0, y:1, collided:false})
+  const drop = () => {
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      //create something when game is over
+      if (player.pos.y < 1) {
+        console.log('Game over!!');
+        setGameOver(true);
+        setDropTime(null);
       }
-      else{
-        //create something when game is over
-        if(player.pos.y < 1){
-          console.log("Game over!!");
-          setGameOver(true);
-          setDropTime(null);
-        }
-        updatePlayerPos({x:0, y:0, collided: true});
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
+  };
+
+  //callback function for keyup event
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000);
       }
-
     }
+  };
 
-    const dropPlayer = () => {
-        drop();
+  const dropPlayer = () => {
+    setDropTime(null);
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        //key code for the left arrow
+        console.log('left');
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        //for the right
+        console.log('right');
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        // for the drop maybe for a space?
+        console.log('drop');
+        dropPlayer();
+      } else if (keyCode === 38) {
+        //this is changing the tetromino
+        playerRotate(stage, 1);
+      }
     }
+  };
 
-    const move = ({keyCode}) => {
-        if(!gameOver){
-            if(keyCode === 37){//key code for the left arrow
-                console.log('left');
-                movePlayer(-1);
-            }
-            else if(keyCode === 39){//for the right
-                console.log('right');
-                movePlayer(1);
-            }
-            else if(keyCode === 40){// for the drop maybe for a space?
-                console.log('drop');
-                dropPlayer();
-            }
-            else if(keyCode === 38){
-              //this is changing the tetromino
-              playerRotate(stage, 1);
-            }
-        }
-    }
+  useInterval(() => {
+    drop();
+  }, dropTime);
 
-  return (//styledTetrisWrapper is there to recognize the click movement from the screen
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+  return (
+    //styledTetrisWrapper is there to recognize the click movement from the screen
+    <StyledTetrisWrapper
+      role='button'
+      tabIndex='0'
+      onKeyDown={e => move(e)}
+      onKeyUp={keyUp}
+    >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
-            {gameOver ? (
-                <Display gameOver={gameOver} text="Game Over" />
-            ) : (
-          <div>
-            <Display text='Score' />
-            <Display text='Rows' />
-            <Display text='Level' />
-          </div>
-            )};
-          <StartButton callback={startGame}/>
+          {gameOver ? (
+            <Display gameOver={gameOver} text='Game Over' />
+          ) : (
+            <div>
+              <Display text='Score' />
+              <Display text='Rows' />
+              <Display text='Level' />
+            </div>
+          )}
+          ;
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,38 +1,55 @@
-import {useState, useEffect} from 'react';//useEffect is cycle method
-import {createStage} from '../gameHelpers';
+import { useState, useEffect } from 'react'; //useEffect is cycle method
+import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
-    const [stage, setStage] = useState(createStage());//setStage creates default stage
+  const [stage, setStage] = useState(createStage()); //setStage creates default stage
+  const [rowsCleared, setRowsCleared] = useState(0);
 
-    useEffect(() => {
-        const updateStage = prevStage => {
-            //First copy the stage
-            const newStage = prevStage.map(row =>
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),//in here, if the cell is already marked with other tetrimonios, then leave it as it is.
-            );
+  useEffect(() => {
+    setRowsCleared(0);
 
-            //Then draw the tetromino
-            player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if(value !== 0){//here if value is not 0, then color it with tetrimonios
-                        newStage[y + player.pos.y][x+player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`,
-                        ]
-                    }
-                })
-            })
-            //then check if we collided
-            if(player.collided){
-                resetPlayer();
-            }
-            return newStage;
-        };
+    const sweepRows = newStage =>
+      newStage.reduce((ack, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          //finding a cell with value 0 is false(-1) then it should be swept away.
+          setRowsCleared(prev => prev + 1); //remove the one
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear'])); //let us add new values to the array(after deleting 3 rows, we need to add 3 more rows on top)
+          return ack;
+        }
+        //if there is somethin
+        ack.push(row); //if we have the full row, we just add it in ack
+        return ack;
+      }, []);
 
-        setStage(prev => updateStage(prev));
+    const updateStage = prevStage => {
+      //First copy the stage
+      const newStage = prevStage.map(
+        row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)) //in here, if the cell is already marked with other tetrimonios, then leave it as it is.
+      );
 
-    }, [player]);//here use effect is somewhat blurry?
-    //[player] is dependency array which is a standard of to use this effect hook when values regarding "player" is changed
+      //Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            //here if value is not 0, then color it with tetrimonios
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`
+            ];
+          }
+        });
+      });
+      //then check if we collided
+      if (player.collided) {
+        resetPlayer();
+        return sweepRows(newStage);
+      }
+      return newStage;
+    };
 
-    return [stage, setStage];
-}
+    setStage(prev => updateStage(prev));
+  }, [player]); //here use effect is somewhat blurry?
+  //[player] is dependency array which is a standard of to use this effect hook when values regarding "player" is changed
+
+  return [stage, setStage, rowsCleared];
+};
